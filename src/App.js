44540284bc88bc6288f43401fcb529f8f6ab26db
Samandar-Umidov <umidovsamandar7@gmail.com>
2024-03-14 00:40:52 +0500

    .

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Overview from './components/overview/Overview'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <Overview />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/switch-button/SwitchButton.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './styles.css'
+class SwitchButton extends Component {
+  handleToggle = () => {
+    const { isChecked, onChange } = this.props;
+    onChange(!isChecked);
+  };
+
+  render() {
+    const { isChecked } = this.props;
+
+    return (
+      <label className={`switch ${isChecked ? 'checked' : ''}`}>
+        <input type="checkbox" checked={isChecked} onChange={this.handleToggle} />
+        <span className="slider"></span>
+      </label>
+    );
+  }
+}
+
+export default SwitchButton;
